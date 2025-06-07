921f9104d49b6d22c19e3d236f9f29496a903469
nandinigit33 <nandinids33@gmail.com>
2025-06-06 20:08:31 -0400

    commit part3 studio

--- part3/studio/src/components/BoardAssignment.jsx
+++ part3/studio/src/components/BoardAssignment.jsx
@@ -1,17 +1,29 @@
 import { useState } from 'react';
 
 export default function BoardAssignment () {
-   const boards = [];
+   const [boardName, setName] = useState('no boards yet!');
+   const boards = [
+      {label: 'Breakfast Ideas', value: 'breakfast'},
+      {label: 'Evening Snacks', value: 'snack'},
+      {label: 'Quick Meals', value: 'quickmeals'}
+               ];
 
    const handleChange = (event) => {
+      setName(event.target.value);
    }
 
    return (
       <div style={{paddingTop: "50px"}}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
-      </select>
-
+    
+      {/* <option value="no boards yet!">-- Select a Board --</option> */}
+            {boards.map((board, index) => (
+               <option key={index} value={board.value}>
+                  {board.label}
+               </option>
+                 ))}
+                   </select>
       <p>Saved to {boardName}!</p>
       </div>
    );
--- part3/studio/src/components/Recipe.jsx
+++ part3/studio/src/components/Recipe.jsx
@@ -1,25 +1,25 @@
 import { useState } from 'react';
 
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://pinchofyum.com/vegan-lettuce-wraps";
+   let authorPhoto = "https://pinchofyum.com/wp-content/assets/images/sidebar/sidebar-lindsay.jpg";
+   let authorName = "Lind Say";
 
    return (
-      <div>
+      <div className='card'>
          <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
          <div>
             <h3>{authorName}</h3>
-            <a href={authorLink}></a> 
+            <a href={authorLink}>{authorLink}</a> 
          </div>
       </div>
    );
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
+   const ingredients = ["peanut butter", "soy sauce", "sesame oil",  "rice vinegar", "Lettuce"];
    return(
-      <div>
+      <div className='card'>
          <h3>Recipe Ingredients</h3>
          <ul>
             <li>{ingredients[0]}</li>
@@ -28,16 +28,23 @@ const RecipeIngredients = () => {
             <li>{ingredients[3]}</li>
             <li>{ingredients[4]}</li>
          </ul>
+         {/* <ul>
+            {ingredients.map((ingredient, index) => (
+               <li key={index}>{ingredient}</li>
+            ))}
+         </ul> */}
       </div>
    );
 }
 
 const RecipeDescription = () => {
+   const title = "Firecracker Vegan Lettuce Wraps";
+   const description = "Firecracker Lettuce Wraps that are happily vegan – with crispy tofu bits, saucy brown rice noodles, and a creamy sesame sauce.";
    return (
       <div> 
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>{title}</h1>
+            <p>{description}</p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
@@ -48,8 +55,9 @@ const RecipeDescription = () => {
 }
 
 const RecipePhoto = () => {
+   const recipeImage = "https://pinchofyum.com/wp-content/uploads/Tofu-Lettuce-Wraps-Recipe-183x183.jpg";
    return (
-      <img src="" alt="" className="imageUpdates"/>
+      <img src={recipeImage} alt={recipeImage} className="imageUpdates"/>
    );
 }
 
